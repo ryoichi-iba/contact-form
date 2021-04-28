e5609e51e5334723f9d1e8fd1c0c6326e7208c4c
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-04-28 18:05:46 +0900

    base code

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+//var_dump($_POST);
+
+$page_flag = 0;
+
+if(!empty($_POST['btn_confirm'])) {
+    $page_flag = 1;
+} elseif(!empty($_POST['btn_submit'])) {
+    $page_flag = 2;
+}
+?>
+
 <!DOCTYPE>
 <html lang="ja">
 <head>
@@ -70,6 +82,29 @@
 </head>
 <body>
 <h1>お問い合わせフォーム</h1>
+
+<?php if ($page_flag === 1): ?>
+
+    <form method="post" action="">
+        <div class="element_wrap">
+            <label>氏名</label>
+            <p><?php echo $_POST['your_name']; ?></p>
+        </div>
+        <div class="element_wrap">
+            <label>メールアドレス</label>
+            <p><?php echo $_POST['email']; ?></p>
+        </div>
+        <input type="submit" name="btn_back" value="戻る">
+        <input type="submit" name="btn_submit" value="送信">
+        <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+        <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+    </form>
+
+<?php elseif( $page_flag === 2 ): ?>
+
+    <p>送信が完了しました。</p>
+
+<?php else: ?>
 <form method="post" action="">
     <div class="element_wrap">
         <label>氏名</label>
@@ -81,5 +116,7 @@
     </div>
     <input type="submit" name="btn_confirm" value="入力内容を確認する">
 </form>
+
+<?php endif; ?>
 </body>
 </html>
